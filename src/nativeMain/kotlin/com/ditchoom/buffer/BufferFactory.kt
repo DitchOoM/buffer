139655084dab02d8184115403ffdefb711040d27
Rahul Behera <thebehera@fb.com>
2022-06-11 15:55:06 -0700

    fix last merge conflict

--- src/nativeMain/kotlin/com/ditchoom/buffer/BufferFactory.kt
+++ src/nativeMain/kotlin/com/ditchoom/buffer/BufferFactory.kt
@@ -4,7 +4,6 @@ package com.ditchoom.buffer
 
 actual fun PlatformBuffer.Companion.allocate(
     size: Int,
-<<<<<<< HEAD
     byteOrder: ByteOrder,
     zone: AllocationZone
 ): PlatformBuffer {
@@ -13,15 +12,9 @@ actual fun PlatformBuffer.Companion.allocate(
     }
     return NativeBuffer(ByteArray(size), byteOrder = byteOrder)
 }
-=======
-    byteOrder: ByteOrder
-): PlatformBuffer = NativeBuffer(ByteArray(size), byteOrder = byteOrder)
->>>>>>> 58e06ae9adb974e31ac1221c7923565fe230da78
-
 actual fun PlatformBuffer.Companion.wrap(array: ByteArray, byteOrder: ByteOrder): PlatformBuffer =
     NativeBuffer(array, byteOrder = byteOrder)
 
-<<<<<<< HEAD
 actual fun String.toBuffer(zone: AllocationZone): PlatformBuffer {
     val bytes = this.encodeToByteArray()
     return if (zone is AllocationZone.Custom) {
@@ -32,7 +25,3 @@ actual fun String.toBuffer(zone: AllocationZone): PlatformBuffer {
         NativeBuffer(bytes, byteOrder = ByteOrder.BIG_ENDIAN)
     }
 }
-=======
-actual fun String.toBuffer(): PlatformBuffer = NativeBuffer(this.encodeToByteArray(), byteOrder = ByteOrder.BIG_ENDIAN)
-actual fun String.utf8Length(): Int = encodeToByteArray().size
->>>>>>> 58e06ae9adb974e31ac1221c7923565fe230da78
