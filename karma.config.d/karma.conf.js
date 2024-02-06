f13cce5f21ae9b5c7902673b02815708d61aa0d6
Rahul Behera <rbehera@gmail.com>
2024-02-05 22:37:04 -0800

    add 10s timeout to browser

---
+++ karma.config.d/karma.conf.js
@@ -0,0 +1,5 @@
+config.client = config.client || {}
+config.client.mocha = config.client.mocha || {}
+config.client.mocha.timeout = 10000
+config.browserNoActivityTimeout = 10000
+config.browserDisconnectTimeout = 10000
